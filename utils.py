d21c453e6f6f48675f1a388f507bc12a2ef0b0dc
Stuart Miller <Stuart.j.miller.8182@gmail.com>
2020-06-15 08:01:13 -0500

    Improve finding files for data generation Update example

--- utils.py
+++ utils.py
@@ -1,59 +1,59 @@
 import glob
 import json
+from collections import defaultdict
+from pathlib import Path
 
 from PIL import Image
 import numpy as np
 from tensorflow.keras.utils import Sequence
 
 
-class batch_generator(Sequence):
+class ImageMetaDataGenerator(Sequence):
+    """Keras-like data generator for image and metadata
+    """
     
     def __init__(self,
                  path,
                  batch_size,
                  target_size,
-                 scaling = (1. / 255)) -> None:
+                 scaling = (1. / 255),
+                 sample_frac = None) -> None:
         """Constructor
         
         :param path: path to data tree
         :param batch_size: size of batches used in training
         :param target_size: image size
         :param scaling: image scaling value
+        :param sample_frac: per class sampling
         """
         self.path = path
         self.batch_size = batch_size
-        self.batches = None
-        self.files = None
-        self.json_to_image_mapping = dict()
-        self.label_mapping = dict()
-        self.classes = None
         self.target_size = target_size
         self.scaling = scaling
-
-        # get the file pathes
-        self.files = glob.glob(self.path + '*/*.json', recursive=True)
-        # map the json file to the image file
-        for file in self.files:
-            # get the json and image file
-            tmp_files = glob.glob(file.replace('.json', '*'))
-            for tf in tmp_files:
-                if tf[-4:] != 'json':
-                    # and make mapping
-                    self.json_to_image_mapping[file] = tf
-                    break
+        self.sample_frac = sample_frac
+        
+        self.files = list()
+        self.json_to_image_mapping = dict()
+        self.label_mapping = dict()
+        self.classes = list()
+        
 
         # create a label maker
-        self.classes = set(glob.glob(self.path + '*'))
-        for i, label in enumerate(self.classes):
+        classes = set(glob.glob(self.path + '*'))
+        for i, label in enumerate(classes):
             label = label.split('/')[-1]
+            self.classes.append(label)
             self.label_mapping[label] = i
-
-        # get a set of indicies
-        idx = np.arange(len(self.files))
-        # shuffle the input files
-        np.random.shuffle(self.files)
-        # get batch indicies
-        self.batches = np.array_split(idx, len(self.files) // self.batch_size)
+        
+        # get the file pathes
+        # sample on a per class basis
+        for class_path in classes:
+            class_files = glob.glob(class_path + '/*.*')
+            if self.sample_frac is not None:
+                class_files = np.random.choice(class_files,
+                                               np.floor(len(class_files) * self.sample_frac).astype(int),
+                                               replace = False)
+            self.files += list(class_files)
 
         print(f"Found {len(self.files)} instances belonging to {len(self.classes)} classes")
 
@@ -65,35 +65,35 @@ class batch_generator(Sequence):
     def __getitem__(self, index):
         """Generate one batch of data
         """
-        for batch in self.batches:
-            images, descriptions, titles = list(), list(), list()
-            labels = list()
-            
-            f_batch = self.files[index * self.batch_size:(index + 1) * self.batch_size]
-            for f in f_batch:
-                # get the text data
-                with open(f, 'r') as in_file:
-                    items = json.load(in_file)
-                    descriptions.append(items['description'])
-                    titles.append(items['title'])
-                # get the image
-                img = Image.open(self.json_to_image_mapping[f])
-                img = img.resize(self.target_size).convert('RGB')
-                img = np.array(img) * self.scaling
-                images.append(img)
-                # get the label
-                labels.append(
-                    self.label_mapping[
-                        f.split('/')[-2]
-                    ]
-                )
-            
-            img_reshape_size = [len(f_batch)] + list(self.target_size) + [3]
-            
-            return ([np.array(images).reshape(*img_reshape_size),
-                   np.array(descriptions),
-                   np.array(titles)],
-                   np.array(labels))
+        
+        images, descriptions, titles = list(), list(), list()
+        labels = list()
+
+        f_batch = self.files[index * self.batch_size:(index + 1) * self.batch_size]
+        for f in f_batch:
+            # get the text data
+            with open(Path(f).with_suffix(''), 'r') as in_file:
+                items = json.load(in_file)
+                descriptions.append(items['description'])
+                titles.append(items['title'])
+            # get the image
+            img = Image.open(f)
+            img = img.resize(self.target_size).convert('RGB')
+            img = np.array(img) * self.scaling
+            images.append(img)
+            # get the label
+            labels.append(
+                self.label_mapping[
+                    f.split('/')[-2]
+                ]
+            )
+
+        img_reshape_size = [len(f_batch)] + list(self.target_size) + [3]
+
+        return ([np.array(images).reshape(*img_reshape_size),
+               np.array(descriptions),
+               np.array(titles)],
+               np.array(labels))
     
     def on_epoch_end(self):
         """Updates indexes after each epoch
