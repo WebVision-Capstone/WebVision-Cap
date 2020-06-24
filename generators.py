f0414a0b74598ff2c1d1884db82b5bc51a4baea1
Stuart Miller <Stuart.j.miller.8182@gmail.com>
2020-06-23 22:22:20 -0500

    Update notebook with inception model Add generators Update utility file

---
+++ generators.py
@@ -0,0 +1,280 @@
+"""Data generators
+"""
+
+import glob
+import os
+import json
+from pathlib import Path
+from typing import Tuple, List
+
+from PIL import Image
+import numpy as np
+from tensorflow.keras.utils import Sequence
+
+class BaseDataGenerator(Sequence):
+    """Keras-like data generator for image and metadata
+    """
+    
+    def __init__(self,
+                 path: str,
+                 batch_size: int,
+                 target_size: Tuple[int, int],
+                 scaling: float = (1. / 255),
+                 sample_frac: float = None) -> None:
+        """Constructor
+        
+        :param path: path to data tree
+        :param batch_size: size of batches used in training
+        :param target_size: image size
+        :param scaling: image scaling value
+        :param sample_frac: per class sampling
+        """
+        # arguments
+        self.path = path
+        self.batch_size = batch_size
+        self.target_size = target_size
+        self.scaling = scaling
+        self.sample_frac = sample_frac
+        # derived
+        self.files = list()
+        self.json_to_image_mapping = dict()
+        self.label_mapping = dict()
+        self.classes = list()
+
+        # create a label maker
+        # targeting sparse categorical crossentropy
+        classes = set(glob.glob(self.path + '*'))
+        for i, label in enumerate(classes):
+            label = label.split('/')[-1]
+            self.classes.append(label)
+            self.label_mapping[label] = i
+        
+        # get the file pathes
+        # sample on a per class basis
+        for class_path in classes:
+            class_files = glob.glob(class_path + '/*.*')
+            if self.sample_frac is not None:
+                class_files = np.random.choice(
+                    class_files,
+                    np.floor(len(class_files) * self.sample_frac).astype(int),
+                    replace = False
+                    )
+            self.files += list(class_files)
+
+        print(f"Found {len(self.files)} instances belonging to {len(self.classes)} classes")
+
+    @property
+    def num_classes(self) -> int:
+        """The number of classes found
+        """
+        return len(self.classes)
+
+    @property
+    def num_instances(self) -> int:
+        """The number of classes found
+        """
+        return len(self.files)
+
+    def _load_metadata(self, path: str) -> Tuple[str, str]:
+        """Load metadata from the JSON files
+        """
+        with open(Path(path).with_suffix(''), 'r') as in_file:
+            items = json.load(in_file)
+            return items['description'], items['title']
+
+    def __len__(self) -> int:
+        """Denotes the number of batches per epoch
+        """
+        return int(np.floor(len(self.files) / self.batch_size))
+
+    def on_epoch_end(self) -> None:
+        """Updates indexes after each epoch
+        """
+        np.random.shuffle(self.files)
+
+class ImageMetaDataGenerator(BaseDataGenerator):
+    """Keras-like data generator for image and metadata
+    """
+    
+    def __init__(self,
+                 path: str,
+                 batch_size: int,
+                 target_size: Tuple[int, int],
+                 scaling: float = (1. / 255),
+                 sample_frac: float = None) -> None:
+        """Constructor
+        
+        :param path: path to data tree
+        :param batch_size: size of batches used in training
+        :param target_size: image size
+        :param scaling: image scaling value
+        :param sample_frac: per class sampling
+        """
+        super().__init__(path,
+                         batch_size,
+                         target_size,
+                         scaling,
+                         sample_frac)
+    
+    def _load_image(self, path: str) -> np.ndarray:
+        """Load image convert to RGB and resize
+        """
+        img = Image.open(path)
+        img = img.resize(self.target_size).convert('RGB')
+        img = np.array(img) * self.scaling
+        return img
+    
+    def __getitem__(self, index: int) -> Tuple[List[np.ndarray], np.ndarray]:
+        """Generate one batch of data
+
+        Returned as ([Image, Description, Title], label)
+        """
+        labels = list()
+        images = list()
+        descriptions = list()
+        titles = list()
+        
+        # get a batch
+        f_batch = self.files[index * self.batch_size:(index + 1) * self.batch_size]
+        
+        # prepare the batch
+        for f in f_batch:
+            # get the text data
+            desc, titl = self._load_metadata(f)
+            descriptions.append(desc)
+            titles.append(titl)
+            # get the image
+            images.append(
+                self._load_image(f)
+            )
+            # get the label
+            labels.append(
+                self.label_mapping[
+                    f.split('/')[-2]
+                ]
+            )
+
+        img_reshape_size = [len(f_batch)] + list(self.target_size) + [3]
+        
+        # images, descriptions, titles, labels
+        return ([np.array(images).reshape(*img_reshape_size),
+               np.array(descriptions),
+               np.array(titles)],
+               np.array(labels))
+
+class MetaDataGenerator(BaseDataGenerator):
+    """Keras-like data generator for metadata
+    """
+    
+    def __init__(self,
+                 path: str,
+                 batch_size: int,
+                 target_size: Tuple[int, int],
+                 scaling: float = (1. / 255),
+                 sample_frac: float = None) -> None:
+        """Constructor
+        
+        :param path: path to data tree
+        :param batch_size: size of batches used in training
+        :param target_size: image size
+        :param scaling: image scaling value
+        :param sample_frac: per class sampling
+        """
+        super().__init__(path,
+                         batch_size,
+                         (300, 300),
+                         1. / 255,
+                         sample_frac)
+    
+    
+    def __getitem__(self, index: int) -> Tuple[List[np.ndarray], np.ndarray]:
+        """Generate one batch of data
+
+        (Returned as [Description, Title], label)
+        """
+        labels = list()
+        descriptions = list()
+        titles = list()
+        
+        # get a batch
+        f_batch = self.files[index * self.batch_size:(index + 1) * self.batch_size]
+        
+        # prepare the batch
+        for f in f_batch:
+            # get the text data
+            desc, titl = self._load_metadata(f)
+            descriptions.append(desc)
+            titles.append(titl)
+            # get the label
+            labels.append(
+                self.label_mapping[
+                    f.split('/')[-2]
+                ]
+            )
+        
+        # images, descriptions, titles, labels
+        return ([np.array(descriptions),
+               np.array(titles)],
+               np.array(labels))
+
+class ImageGenerator(BaseDataGenerator):
+    """Keras-like data generator for image and metadata
+    """
+    
+    def __init__(self,
+                 path: str,
+                 batch_size: int,
+                 target_size: Tuple[int, int],
+                 scaling: float = (1. / 255),
+                 sample_frac: float = None) -> None:
+        """Constructor
+        
+        :param path: path to data tree
+        :param batch_size: size of batches used in training
+        :param target_size: image size
+        :param scaling: image scaling value
+        :param sample_frac: per class sampling
+        """
+        super().__init__(path,
+                         batch_size,
+                         target_size,
+                         scaling,
+                         sample_frac)
+    
+    def _load_image(self, path: str) -> np.ndarray:
+        """Load image convert to RGB and resize
+        """
+        img = Image.open(path)
+        img = img.resize(self.target_size).convert('RGB')
+        img = np.array(img) * self.scaling
+        return img
+    
+    def __getitem__(self, index: int) -> Tuple[List[np.ndarray], np.ndarray]:
+        """Generate one batch of data
+
+        Returned as (Image, label)
+        """
+        labels = list()
+        images = list()
+        
+        # get a batch
+        f_batch = self.files[index * self.batch_size:(index + 1) * self.batch_size]
+        
+        # prepare the batch
+        for f in f_batch:
+            # get the image
+            images.append(
+                self._load_image(f)
+            )
+            # get the label
+            labels.append(
+                self.label_mapping[
+                    f.split('/')[-2]
+                ]
+            )
+
+        img_reshape_size = [len(f_batch)] + list(self.target_size) + [3]
+        
+        # images, descriptions, titles, labels
+        return (np.array(images).reshape(*img_reshape_size),
+               np.array(labels))
--- utils.py
+++ utils.py
@@ -1,101 +1,32 @@
-import glob
-import json
-from collections import defaultdict
-from pathlib import Path
+"""General utilities
+"""
 
-from PIL import Image
-import numpy as np
-from tensorflow.keras.utils import Sequence
+import tensorflow as tf
+from typing import List
 
-
-class ImageMetaDataGenerator(Sequence):
-    """Keras-like data generator for image and metadata
+def plot_accuracy_loss(history: tf.keras.callbacks.History,
+                       acc_items: List[str],
+                       loss_items: List[str]):
+    """Plot accuracy metrics and loss metrics from training history
+    
+    Usage:
+    plot_accuracy_loss(
+                       history,
+                       ['accuracy', 'val_accuracy'],
+                       ['loss', 'val_loss'])
+    
     """
+    fig, ax = plt.subplots(ncols = 2, figsize = (15, 7))
+    fig.suptitle('Accuracy and Loss')
     
-    def __init__(self,
-                 path,
-                 batch_size,
-                 target_size,
-                 scaling = (1. / 255),
-                 sample_frac = None) -> None:
-        """Constructor
-        
-        :param path: path to data tree
-        :param batch_size: size of batches used in training
-        :param target_size: image size
-        :param scaling: image scaling value
-        :param sample_frac: per class sampling
-        """
-        self.path = path
-        self.batch_size = batch_size
-        self.target_size = target_size
-        self.scaling = scaling
-        self.sample_frac = sample_frac
-        
-        self.files = list()
-        self.json_to_image_mapping = dict()
-        self.label_mapping = dict()
-        self.classes = list()
-        
-
-        # create a label maker
-        classes = set(glob.glob(self.path + '*'))
-        for i, label in enumerate(classes):
-            label = label.split('/')[-1]
-            self.classes.append(label)
-            self.label_mapping[label] = i
-        
-        # get the file pathes
-        # sample on a per class basis
-        for class_path in classes:
-            class_files = glob.glob(class_path + '/*.*')
-            if self.sample_frac is not None:
-                class_files = np.random.choice(class_files,
-                                               np.floor(len(class_files) * self.sample_frac).astype(int),
-                                               replace = False)
-            self.files += list(class_files)
-
-        print(f"Found {len(self.files)} instances belonging to {len(self.classes)} classes")
-
-    def __len__(self):
-        """Denotes the number of batches per epoch
-        """
-        return int(np.floor(len(self.files) / self.batch_size))
+    for item in acc_items:
+        ax[0].plot(history.history[item], label = item);
+    ax[0].set_xlabel('Epochs');
+    ax[0].set_ylabel('Accuracy');
+    ax[0].legend();
     
-    def __getitem__(self, index):
-        """Generate one batch of data
-        """
-        
-        images, descriptions, titles = list(), list(), list()
-        labels = list()
-
-        f_batch = self.files[index * self.batch_size:(index + 1) * self.batch_size]
-        for f in f_batch:
-            # get the text data
-            with open(Path(f).with_suffix(''), 'r') as in_file:
-                items = json.load(in_file)
-                descriptions.append(items['description'])
-                titles.append(items['title'])
-            # get the image
-            img = Image.open(f)
-            img = img.resize(self.target_size).convert('RGB')
-            img = np.array(img) * self.scaling
-            images.append(img)
-            # get the label
-            labels.append(
-                self.label_mapping[
-                    f.split('/')[-2]
-                ]
-            )
-
-        img_reshape_size = [len(f_batch)] + list(self.target_size) + [3]
-
-        return ([np.array(images).reshape(*img_reshape_size),
-               np.array(descriptions),
-               np.array(titles)],
-               np.array(labels))
-    
-    def on_epoch_end(self):
-        """Updates indexes after each epoch
-        """
-        np.random.shuffle(self.files)
+    for item in loss_items:
+        ax[1].plot(history.history[item], label = item);
+    ax[1].set_xlabel('Epochs');
+    ax[1].set_ylabel('Loss');
+    ax[1].legend();
